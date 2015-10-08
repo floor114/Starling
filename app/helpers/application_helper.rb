3ecbdf83f7613cc2b85885773c353d0022c30116
Taras Shpachenko <taras.shpachenko@yandex.ru>
2015-10-08 14:33:53 +0300

    add some changes in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
     end
   end
 
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
