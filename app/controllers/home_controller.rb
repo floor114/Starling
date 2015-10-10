6ec171a60c88bee2e7bbebdc057271886a12d107
Taras Shpachenko <taras.shpachenko@yandex.ru>
2015-10-10 15:00:40 +0300

    add user edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       redirect_to user_path(current_user)
-    else
-      
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :users
+  resources :users, only: [:show, :edit, :update]
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
