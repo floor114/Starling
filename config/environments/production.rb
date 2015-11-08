f997a2b0bdfc316e4af096cf342a164bb3e7873b
Taras Shpachenko <taras.shpachenko@yandex.ru>
2015-11-08 22:33:07 +0200

    Sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,11 @@ Rails.application.configure do
   config.action_mailer.default :charset => 'utf-8'
 
   config.action_mailer.smtp_settings = {
-      address:        'smtp.gmail.com',
-      domain:         ENV['GMAIL_DOMAIN'],
+      address:        'smtp.sendgrid.net',
+      domain:         'heroku.com',
       port:           587,
-      user_name:      ENV['GMAIL_USERNAME'],
-      password:       ENV['GMAIL_PASSWORD'],
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
       authentication: 'plain',
       enable_starttls_auto: true
   }
