d100f80714d508d811f2cfd894b6c1ae2c5f5747
Taras Shpachenko <taras.shpachenko@yandex.ru>
2015-11-11 09:25:39 +0200

    Sendgrid fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => 'utf-8'
 
   config.action_mailer.smtp_settings = {
-      address:        'smtp.gmail.com',
+      address:        'smtp.sendgrid.net',
       domain:         'heroku.com',
       port:           587,
       user_name:      ENV['SENDGRID_USERNAME'],
